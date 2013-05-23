bff7a6aa1bdad261e83eb8f113aa706f687d8b6a
serb <serb@tut.by>
2013-05-23 21:53:11 +0400

    Implemented returning a value from the Add Feed dialog.

--- src/com/serb/podpamp/ui/AddFeedDialog.java
+++ src/com/serb/podpamp/ui/AddFeedDialog.java
@@ -1,22 +1,55 @@
 package com.serb.podpamp.ui;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.InputType;
+import android.widget.EditText;
 import com.serb.podpamp.R;
 
 public class AddFeedDialog extends DialogFragment {
+	public interface AddFeedDialogListener {
+		public void onDialogPositiveClick(String feed_url);
+	}
+
+
+
+	AddFeedDialogListener mListener;
+
+
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		// Verify that the host activity implements the callback interface
+		try {
+			mListener = (AddFeedDialogListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString() + " must implement AddFeedDialogListener");
+		}
+	}
+
+
+
+
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		final EditText input = new EditText(getActivity());
+		input.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
+		input.setText("http://");
+
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		builder.setMessage(R.string.add_feed_dialog_message)
 			.setTitle(R.string.add_feed_dialog_title)
+			.setView(input)
 			.setPositiveButton(R.string.add_button_text, new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int id) {
-
+					mListener.onDialogPositiveClick(String.valueOf(input.getText()));
 				}
 			})
 			.setNegativeButton(R.string.cancel_button_text, new DialogInterface.OnClickListener() {
--- src/com/serb/podpamp/ui/FeedsActivity.java
+++ src/com/serb/podpamp/ui/FeedsActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.TextView;
 import com.serb.podpamp.R;
 
-public class FeedsActivity extends FragmentActivity implements View.OnClickListener {
+public class FeedsActivity extends FragmentActivity implements View.OnClickListener,
+		AddFeedDialog.AddFeedDialogListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,11 +29,24 @@ public class FeedsActivity extends FragmentActivity implements View.OnClickListe
 	}
 
 
+
+	@Override
+	public void onDialogPositiveClick(String feed_url) {
+		if (feed_url.length() > 0 && feed_url.startsWith("http"))
+			addFeed(feed_url);
+	}
+
 	//region Private Methods.
 
 	private void showAddFeed() {
 		DialogFragment newFragment = new AddFeedDialog();
 		newFragment.show(getSupportFragmentManager(), "add_feed");
+	}
+
+
+
+	private void addFeed(String feed_url) {
+		((TextView)findViewById(R.id.txt_greet)).setText(feed_url);
 	}
 
 	//endregion
