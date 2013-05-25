8ce15b7860228165dfcbb1591da56c1622910f47
serb <serb@tut.by>
2013-05-25 11:54:02 +0400

    Implemented ability to read rss feeds.

--- src/com/serb/podpamp/model/operations/AddFeedOperation.java
+++ src/com/serb/podpamp/model/operations/AddFeedOperation.java
@@ -10,18 +10,34 @@ import com.foxykeep.datadroid.requestmanager.Request;
 import com.foxykeep.datadroid.service.RequestService;
 import com.serb.podpamp.model.provider.Contract;
 import com.serb.podpamp.model.request.RequestFactory;
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSItem;
+import org.mcsoxford.rss.RSSReader;
+import org.mcsoxford.rss.RSSReaderException;
+
+import java.util.List;
 
 public class AddFeedOperation implements RequestService.Operation {
 	@Override
 	public Bundle execute(Context context, Request request)
 		throws ConnectionException, DataException, CustomRequestException {
 		String url = request.getString(RequestFactory.FEED_URL);
-		ContentValues feed = new ContentValues();
-		feed.put(Contract.FeedsColumns.TITLE, "Test Title");
-		feed.put(Contract.FeedsColumns.URL, url);
-		feed.put(Contract.FeedsColumns.NEW_ITEMS_COUNT, 10);
 
-		context.getContentResolver().insert(Contract.Feeds.CONTENT_URI, feed);
+		RSSReader reader = new RSSReader();
+
+		try {
+			RSSFeed rss_feed = reader.load(url);
+			List<RSSItem> items = rss_feed.getItems();
+
+			ContentValues feed = new ContentValues();
+			feed.put(Contract.FeedsColumns.TITLE, rss_feed.getTitle());
+			feed.put(Contract.FeedsColumns.URL, url);
+			feed.put(Contract.FeedsColumns.NEW_ITEMS_COUNT, items.size());
+
+			context.getContentResolver().insert(Contract.Feeds.CONTENT_URI, feed);
+		} catch (RSSReaderException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 }
